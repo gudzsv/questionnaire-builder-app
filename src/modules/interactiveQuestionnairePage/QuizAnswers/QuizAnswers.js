fd613f45b6b5297af67a75fce925e54ffdc0a6af
gudzsv <gudzsvmail@gmail.com>
2025-03-23 21:49:46 +0100

    remove duplicate return from QuizAnswers component

--- src/modules/interactiveQuestionnairePage/QuizAnswers/QuizAnswers.js
+++ src/modules/interactiveQuestionnairePage/QuizAnswers/QuizAnswers.js
@@ -28,27 +28,4 @@ export default function QuizAnswers({ questionId: qId, type, answers }) {
 			})}
 		</ol>
 	);
-	return (
-		<ol>
-			{answers.map(({ _id: aId, answer }) => {
-				const inputType =
-					type === 'single'
-						? 'radio'
-						: type === 'multiple'
-						? 'checkbox'
-						: 'text';
-				return (
-					<li key={aId}>
-						<input
-							type={inputType}
-							id={aId}
-							name={questionId}
-							value={type === 'text' ? undefined : aId}
-						/>
-						{type !== 'text' && <label htmlFor={aId}>{answer}</label>}
-					</li>
-				);
-			})}
-		</ol>
-	);
 }
